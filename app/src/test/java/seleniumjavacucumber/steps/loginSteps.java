21bfb34c6b2b5d81995f225e8a8779ab855d05c7
JIMGA8 <jimga8@gmail.com>
2023-07-25 15:55:59 -0300

    definicion de Steps

--- app/src/test/java/seleniumjavacucumber/steps/loginSteps.java
+++ app/src/test/java/seleniumjavacucumber/steps/loginSteps.java
@@ -1,11 +1,23 @@
 package seleniumjavacucumber.steps;
 
 import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
 
 public class loginSteps {
 
-    @Given("que navego a la pagina saucedemo")
+    @Given("^que navego a la pagina saucedemo$")
     public void navegarASaucedemo() {
+
+    }
+
+    @When("^ingreso el usuario (.+) y la contrasena (.+)$")
+    public void login(String usuario, String contrasena) {
+
+    }
+
+    @Then("^valido el login$")
+    public void nada() {
 
     }
 
